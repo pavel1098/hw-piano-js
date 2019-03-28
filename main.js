b2546330eadefd36582cc86c66aa5ee6d951edf5
Pavlo Mykhaleiko <37704429+pavel1098@users.noreply.github.com>
2019-03-28 17:26:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const piano = document.getElementsByClassName('piano-key');
+
+
+function playSound(sound) {
+	sound.currentTime = 0;
+	sound.play();
+}
+
+ 
+function playByKlick(element) {
+ 	element.addEventListener('mousedown', function() {
+ 		playSound(element.childNodes[3]);
+ 		element.classList.add('active');
+ 		element.addEventListener('mouseup', function() {
+ 		element.classList.remove('active');
+ 		});
+ 	}); 	
+ }
+
+
+
+function playByKey(element) {
+ 	document.addEventListener('keypress', function(event) {
+ 	const letterKey = element.childNodes[1].textContent.toLowerCase().charCodeAt();
+
+ 	if(event.keyCode === letterKey) {
+ 		element.classList.add('active');
+		playSound(element.childNodes[3]);
+		document.addEventListener('keyup', function() {
+ 			element.classList.remove('active');
+ 		})
+ 	}
+ 	});
+}
+
+
+for (let i = 0; i < piano.length; i++) {
+	playByKlick(piano[i]);
+	playByKey(piano[i]);
+}
+
